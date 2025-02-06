06917071f3d750e8d37b2c19b366b5bdf8d838bc
tonovarela <tonovarela@live.com>
2025-02-05 18:15:56 -0600

    Agregar componentes de página de issues y lista de issues, configurar Tailwind CSS y actualizar rutas

---
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -0,0 +1,9 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-page',
+  imports: [],
+  templateUrl: './issue-page.component.html',
+  
+})
+export default class IssuePageComponent { }
---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-list-page',
+  imports: [],
+  templateUrl: './issues-list-page.component.html',  
+})
+export default class IssuesListPageComponent { }
